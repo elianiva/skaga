17a34ad3d93bdc635afc3d3867243d32f331b807
elianiva <dicha.arkana03@gmail.com>
2020-06-29 01:51:33 +0700

    added Hero component

---
+++ parts/Hero.js
@@ -0,0 +1,18 @@
+export default function Hero() {
+  return (
+    <div
+      className="relative w-full z-0 nunito flex flex-col justify-center text-center text-white"
+      style={{ height: `29rem` }}
+    >
+      <img
+        src="/main_cover.jpg"
+        alt="Foto SMKN 3 Jember"
+        className="absolute w-full h-full z-0 object-cover"
+      />
+      <span className="text-white z-10 text-lg">Selamat datang di website</span>
+      <h1 className="text-white font-bold text-5xl z-10">
+        SMK Negeri 3 Jember
+      </h1>
+    </div>
+  )
+}
