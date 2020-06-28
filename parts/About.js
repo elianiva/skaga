7579d3bc8bfa6af0a358da4035661bdfa2a8a45f
elianiva <dicha.arkana03@gmail.com>
2020-06-29 01:51:43 +0700

    added About component for Homepage

---
+++ parts/About.js
@@ -0,0 +1,25 @@
+import Button from "../components/Button"
+
+export default function About() {
+  return (
+    <div className="grid grid-cols-12 gap-4 my-8">
+      <div className="col-span-6">
+        <img
+          src="/about_cover.jpg"
+          alt="Foto SMKN 3 Jember"
+          className="w-11/12 h-full"
+        />
+      </div>
+      <div className="col-span-6 nunito flex flex-col justify-center items-start">
+        <h1 className="text-2xl leading-snug">Tentang</h1>
+        <h2 className="text-4xl font-semibold">SMKN 3 Jember</h2>
+        <p className="text-gray-700 open-sans leading-loose">
+          SMKN 3 Jember merupakan sekolah menengah kejuruan yg berada di Jember
+          dengan akreditasi A. Siswa tidak hanya diajari teori namun juga
+          diajarkan berbagai macam praktek yang akan berguna di dunia industri.
+        </p>
+        <Button className="mt-4" />
+      </div>
+    </div>
+  )
+}
