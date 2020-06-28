4a6171225c91400f08e9dd8fafa715216155760d
elianiva <dicha.arkana03@gmail.com>
2020-06-29 01:49:30 +0700

    initialized project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: ["./**/*.js"],
+  theme: {
+    boxShadow: {
+      orange: ".25rem .25rem .25rem rgba(244, 138, 78, .16)",
+      orangeHover: ".25rem .25rem .5rem rgba(255, 170, 121, .5)"
+    },
+    extend: {
+      colors: {
+        orange: "#f48a4e",
+        orangeHover: "#FF9849"
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
