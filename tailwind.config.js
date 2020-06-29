eb98a37733d429a249eb9986f36e2a2ccbbc9647
elianiva <dicha.arkana03@gmail.com>
2020-06-29 21:17:32 +0700

    extended tailwind variation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
 module.exports = {
   purge: ["./**/*.js"],
   theme: {
-    boxShadow: {
-      orange: ".25rem .25rem .25rem rgba(244, 138, 78, .16)",
-      orangeHover: ".25rem .25rem .5rem rgba(255, 170, 121, .5)"
-    },
     extend: {
       colors: {
         orange: "#f48a4e",
         orangeHover: "#FF9849",
+        lightOrange: "#FAC2A2",
+        darkOrange: "#893C10",
         blue: {
           "900": "#222F48"
         }
+      },
+      boxShadow: {
+        orange: ".25rem .25rem .25rem rgba(244, 138, 78, .16)",
+        orangeHover: ".25rem .25rem .5rem rgba(255, 170, 121, .5)"
       }
     }
   },
