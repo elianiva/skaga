c4f6a2d07304c4f1cceb7e5bc18e68fab763365e
elianiva <dicha.arkana03@gmail.com>
2020-06-29 14:02:21 +0700

    added href for button

--- components/Button.js
+++ components/Button.js
@@ -1,10 +1,14 @@
-export default function Button({ children, className }) {
+import Link from "next/link"
+
+export default function Button({ children, className, href }) {
   return (
-    <button
-      className={`px-4 py-2 rounded bg-orange text-white shadow-orange hover:shadow-orangeHover hover:bg-orangeHover hover:bg- ${className}`}
-      style={{ transition: `all ease-out .2s` }}
-    >
-      Selengkapnya
-    </button>
+    <Link href={href}>
+      <a
+        className={`px-4 py-2 rounded bg-orange text-white shadow-orange hover:shadow-orangeHover hover:bg-orangeHover hover:bg- ${className}`}
+        style={{ transition: `all ease-out .2s` }}
+      >
+        Selengkapnya
+      </a>
+    </Link>
   )
 }
