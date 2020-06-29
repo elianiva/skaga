4eee68ece285fee9fcc8db6c0ab4e717d0b3f288
elianiva <dicha.arkana03@gmail.com>
2020-06-29 13:36:41 +0700

    fixed card margin/padding

--- components/Card.js
+++ components/Card.js
@@ -4,14 +4,12 @@ export default function Card({ imgUrl, alt, jurusan, deskripsi }) {
   return (
     <div className="grid grid-cols-12 gap-4 items-center mb-12">
       <img src={imgUrl} alt={alt} className="col-span-5 rounded-md" />
-      <div className="col-span-7 flex flex-col items-start">
+      <div className="col-span-7 flex flex-col items-start ml-4">
         <span className="nunito text-lg text-blue-900">
           Kompetensi Keahlian
         </span>
-        <h1 className="nunito text-3xl text-blue-900 font-semibold">
-          {jurusan}
-        </h1>
-        <p className="open-sans text-gray-700 leading-loose mb-4">
+        <h1 className="nunito text-3xl text-blue-900 mb-2">{jurusan}</h1>
+        <p className="open-sans text-gray-700 leading-relaxed mb-4">
           {deskripsi}
         </p>
         <Button />
