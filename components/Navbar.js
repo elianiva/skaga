58dea99945cd209f0bef43de7ced10c98b386a2d
elianiva <dicha.arkana03@gmail.com>
2020-06-29 12:34:35 +0700

    conditional navbar styling based on route

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,21 +1,30 @@
 import { useEffect, useState } from "react"
+import { useRouter } from "next/router"
 import Link from "next/link"
 import Arrow from "../public/arrow.svg"
 
 export default function Navbar({ currentPage }) {
+  const router = useRouter()
   const [offsetY, setOffsetY] = useState(0)
 
   useEffect(() => {
     window.addEventListener("scroll", () => setOffsetY(window.pageYOffset))
   }, [])
 
+  const uwu =
+    "fixed z-10 t-0 inset-x-0 h-16 nunito bg-white text-blue-900 border-b-2 border-gray-300"
+
   return (
     <nav
-      className={`fixed z-10 t-0 inset-x-0 h-16 nunito ${
-        offsetY >= 32
-          ? "bg-white text-blue-900 border-b-2 border-gray-100"
-          : "text-white"
-      }`}
+      className={
+        router.pathname === "/"
+          ? `fixed z-10 t-0 inset-x-0 h-16 nunito ${
+              offsetY >= 32
+                ? "bg-white text-blue-900 border-b-2 border-gray-300"
+                : "text-white"
+            }`
+          : "fixed z-10 t-0 inset-x-0 h-16 nunito bg-white text-blue-900 border-b-2 border-gray-300"
+      }
       style={{
         transition: `background-color ease-out .25s`
       }}
@@ -25,17 +34,32 @@ export default function Navbar({ currentPage }) {
         <div className="grid grid-cols-5 justify-center items-center gap-4 font-semibold tracking-wider">
           <div className="flex items-center justify-center cursor-pointer hover:text-orange">
             <Link href="/">
-              <a title="Beranda">Beranda</a>
+              <a
+                title="Beranda"
+                className={router.pathname === "/" && "active"}
+              >
+                Beranda
+              </a>
             </Link>
           </div>
           <div className="flex items-center justify-center cursor-pointer hover:text-orange">
             <Link href="/berita">
-              <a title="Berita">Berita</a>
+              <a
+                title="Berita"
+                className={router.pathname === "/berita" && "active"}
+              >
+                Berita
+              </a>
             </Link>
           </div>
           <div className="flex items-center justify-center cursor-pointer hover:text-orange">
             <Link href="/jurusan">
-              <a title="Jurusan">Jurusan</a>
+              <a
+                title="Jurusan"
+                className={router.pathname === "/jurusan" && "active"}
+              >
+                Jurusan
+              </a>
             </Link>
           </div>
           <div className="flex items-center justify-center cursor-pointer">
