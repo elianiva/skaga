6d72cc5316676251417d62698dfaf4911f727f24
elianiva <dicha.arkana03@gmail.com>
2020-06-29 12:36:52 +0700

    added title component for "jurusan" page

---
+++ components/Title.js
@@ -0,0 +1,8 @@
+export default function Title({ title }) {
+  return (
+    <div className="flex flex-col items-center mb-8">
+      <h1 className="text-3xl nunito">{title}</h1>
+      <div className="h-1 w-40 bg-orange rounded"></div>
+    </div>
+  )
+}
