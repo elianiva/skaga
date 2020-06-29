3edbe72a6d95bf059ac2db653fe0c1654f9a4621
elianiva <dicha.arkana03@gmail.com>
2020-06-29 14:02:05 +0700

    moved jurusan.js to jurusan directory

--- pages/profil/jurusan/index.js
+++ pages/profil/jurusan/index.js
@@ -1,7 +1,7 @@
-import Layout from "../../components/Layout"
-import Card from "../../components/Card"
-import Title from "../../components/Title"
-import Breadcrumb from "../../components/Breadcrumb"
+import Layout from "components/Layout"
+import Card from "components/Card"
+import Title from "components/Title"
+import Breadcrumb from "components/Breadcrumb"
 
 export default function JurusanPage() {
   return (
@@ -13,46 +13,55 @@ export default function JurusanPage() {
           imgUrl="/jurusan/perhotelan.jpg"
           jurusan="Perhotelan"
           deskripsi="Perhotelan merupakan bidang ilmu yang mempelajari pengelolaan hotel serta cara menyeimbangkan aspek wisata dan manajemen bisnis untuk mencapai kesuksesan."
+          href="/profil/jurusan/perhotelan"
         />
         <Card
           imgUrl="/jurusan/upw.jpg"
           jurusan="Usaha Perjalanan Wisata"
           deskripsi="Program studi yang mempelajari kegiatan yang bersifat komersial yang memberi pelayanan bagi suatu kelompok yang melakukan perjalanan dengan tujuan utama yaitu berwisata."
+          href="/profil/jurusan/usaha-perjalanan-wisata"
         />
         <Card
           imgUrl="/jurusan/tbg.jpg"
           jurusan="Tata Boga"
           deskripsi="Program studi yang mempelajari teknik penyajian makanan dengan memperhatikan kualitas, rasa, estetika, dan kebutuhan gizi. Kamu akan belajar mengolah berbagai hidangan dari daerah dan negara."
+          href="/profil/jurusan/tata-boga"
         />
         <Card
           imgUrl="/jurusan/tbs.jpg"
           jurusan="Tata Busana"
           deskripsi="Program studi yang mempelajari teknik mendesain, belajar membuat pola, menjahit, dan seluk beluk produksi busana atau fashion serta  analisa tren, bagaimana marketing dan manajemen bisnis fashion."
+          href="/profil/jurusan/tata-busana"
         />
         <Card
           imgUrl="/jurusan/df.jpg"
           jurusan="Desain Fesyen"
           deskripsi="Program studi yang mempelajari ilmu merancang busana. Ada banyak spesialisasi yang bisa dipilih dalam jurusan fashion design. Disini juga diajari cara memasarkan produk, menciptakan tren, dan sebagainya."
+          href="/profil/jurusan/desain-fesyen"
         />
         <Card
           imgUrl="/jurusan/kecantikan.jpg"
           jurusan="Kecantikan Kulit dan Rambut"
           deskripsi="Program studi yang mempelajari tata rias Indonesia dan internasional. Beberapa hal yang dipelajari adalah cara menata rambut, memotong rambut, merias wajah menggunakan bahan dan alat kosmetika."
+          href="/profil/jurusan/kecantikan-kulit-dan-rambut"
         />
         <Card
           imgUrl="/jurusan/tkj.jpg"
           jurusan="Teknologi Komputer dan Jaringan"
           deskripsi="Program studi yang mempelajari tentang cara-cara merakit komputer dan menginstalasi program komputer. Program studi ini juga membahas hal yang berkaitan dengan jaringan."
+          href="/profil/jurusan/teknologi-komputer-dan-jaringan"
         />
         <Card
           imgUrl="/jurusan/rpl.jpg"
           jurusan="Rekayasa Perangkat Lunak"
           deskripsi="Program studi yang mempelajari tentang cara-cara merakit komputer dan menginstalasi program komputer. Program studi ini juga membahas hal yang berkaitan dengan jaringan."
+          href="/profil/jurusan/rekayasa-perangkat-lunak"
         />
         <Card
           imgUrl="/jurusan/mm.jpg"
           jurusan="Multimedia"
           deskripsi="Program studi yang mempelajari berkaitan dengan penggunaan berbagai media untuk menyampaikan informasi ke publik, termasuk media digital dan media cetak."
+          href="/profil/jurusan/multimedia"
         />
       </div>
     </Layout>
