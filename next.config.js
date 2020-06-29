64f790ed63b76fcbb09c6508c52480bc1c9b0b37
elianiva <dicha.arkana03@gmail.com>
2020-06-29 14:01:43 +0700

    set root directory for modules import

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const path = require("path")
 module.exports = withReactSvg({
   include: path.resolve(__dirname, "public"),
   webpack(config, options) {
+    // set root directory for importing modules to ./
+    config.resolve.modules.push(path.resolve("./"))
     return config
   }
 })
