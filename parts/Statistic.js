ecdc0f7c169d8fc9c031e83bae2a75aab9061c74
elianiva <dicha.arkana03@gmail.com>
2020-06-30 16:06:12 +0700

    removed react-countup

--- parts/Statistic.js
+++ parts/Statistic.js
@@ -1,32 +1,28 @@
-import CountUp from "react-countup"
-
-function Line() {
-  return (
-    <div className="bg-orange h-16 rounded" style={{ width: `.125rem` }}></div>
-  )
-}
+const Line = () => (
+  <div className="bg-orange h-16 rounded" style={{ width: `.125rem` }}></div>
+)
 
 export default function Statistic() {
   return (
     <div className="p-8 bg-gray-100 nunito">
       <div className="flex items-center justify-center max-w-5xl m-auto">
         <div className="flex-1 text-center">
-          <CountUp start={0} end={69} duration={4} className="text-5xl" />
+          <span className="text-5xl">69</span>
           <span className="block text-lg">Staf dan Guru</span>
         </div>
         <Line />
         <div className="flex-1 text-center">
-          <CountUp start={0} end={9} duration={4} className="text-5xl" />
+          <span className="text-5xl">9</span>
           <span className="block text-lg">Kompetensi Keahlian</span>
         </div>
         <Line />
         <div className="flex-1 text-center">
-          <CountUp start={0} end={3690} duration={4} className="text-5xl" />
+          <span className="text-5xl">3690</span>
           <span className="block text-lg">Siswa dan Siswi</span>
         </div>
         <Line />
         <div className="flex-1 text-center">
-          <CountUp start={0} end={69} duration={4} className="text-5xl" />
+          <span className="text-5xl">42</span>
           <span className="block text-lg">Ruang Kelas</span>
         </div>
       </div>
