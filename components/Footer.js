3add130c859d7ce2b76d2aa9654a2914413d2569
elianiva <dicha.arkana03@gmail.com>
2020-07-01 00:49:50 +0700

    made the navbar and footer responsive

--- components/Footer.js
+++ components/Footer.js
@@ -7,31 +7,34 @@ import Image from "components/Image"
 export default function Footer() {
   return (
     <footer>
-      <div className="pt-8 border-t-2 border-gray-300">
-        <div className="flex max-w-5xl m-auto items-center">
+      <div className="pt-8 border-t-2 border-gray-300 px-4 lg:px-0">
+        <div className="flex flex-col lg:flex-row max-w-5xl m-auto lg:items-center">
           <div className="flex flex-col flex-1">
-            <div className="flex items-center">
+            <div className="lg:flex lg:items-center text-center">
               <Image
                 src="logo_skaga.jpg"
                 alt="Logo SMKN 3 Jember"
-                className="inline-block w-20 h-20 mr-2"
+                className="inline-block w-32 h-32 lg:w-20 lg:h-20 mr-2 mb-4 lg:mb-0"
               />
               <h3 className="text-2xl nunito font-bold">
-                SMK NEGERI 3
-                <br />
+                SMK NEGERI 3 <br className="hidden lg:block" />
                 JEMBER
               </h3>
             </div>
-            <p className="text-sm text-grey-700 open-sans block mt-4">
+            <p className="lg:text-sm text-grey-700 open-sans block mt-4 text-center lg:text-left">
               SMKN 3 Jember merupakan sekolah menengah kejuruan berbasis
               pariwisata dengan akreditasi A.
             </p>
           </div>
           <div
-            className="h-32 bg-orange rounded mx-8"
+            className="hidden lg:block h-32 bg-orange rounded mx-8"
             style={{ width: `.125rem` }}
           ></div>
-          <div className="nunito flex-1">
+          <div
+            className="lg:hidden block bg-orange rounded my-4 m-auto w-full"
+            style={{ height: `.125rem` }}
+          ></div>
+          <div className="nunito flex-1 lg:py-0">
             <h2 className="font-bold text-xl text-center">Kontak</h2>
             <span className="flex block my-2">
               <PhoneIcon className="w-8" /> 0331 - 488069
@@ -47,8 +50,12 @@ export default function Footer() {
             </span>
           </div>
           <div
-            className="h-32 bg-orange rounded mx-8"
+            className="hidden lg:block h-32 bg-orange rounded mx-8"
             style={{ width: `.125rem` }}
+          ></div>
+          <div
+            className="lg:hidden block bg-orange rounded my-4 m-auto w-full"
+            style={{ height: `.125rem` }}
           ></div>
           <div className="flex-1">
             <Image
@@ -63,7 +70,7 @@ export default function Footer() {
           </div>
         </div>
       </div>
-      <span className="open-sans text-lg my-4 text-center block">
+      <span className="open-sans text-lg mt-4 py-4 text-center block bg-gray-100">
         &copy; 1969 - 2020 SMK Negeri 3 Jember
       </span>
     </footer>
--- components/Navbar.js
+++ components/Navbar.js
@@ -6,58 +6,159 @@ import Arrow from "public/icons/arrow.svg"
 export default function Navbar({ currentPage }) {
   const router = useRouter()
   const [offsetY, setOffsetY] = useState(0)
+  const [isVisible, setVisible] = useState(false)
 
   useEffect(() => {
     window.addEventListener("scroll", () => setOffsetY(window.pageYOffset))
   }, [])
 
   return (
-    <nav
-      className={
-        router.pathname === "/"
-          ? `fixed z-10 t-0 inset-x-0 h-16 nunito ${
-              offsetY >= 32
-                ? "bg-white text-blue-900 border-b-2 border-gray-300"
-                : "text-white"
-            }`
-          : "fixed z-10 t-0 inset-x-0 h-16 nunito bg-white text-blue-900 border-b-2 border-gray-300"
-      }
-      style={{
-        transition: `background-color ease-out .25s`
-      }}
-    >
-      <div className="max-w-5xl flex items-center m-auto h-full">
-        <h1 className="font-extrabold text-2xl flex-1">
-          <Link href="/">
-            <a title="Beranda">SMKN 3 Jember</a>
-          </Link>
-        </h1>
-        <div className="grid grid-cols-5 justify-center items-center gap-4 tracking-wider">
-          <div className="flex items-center justify-center cursor-pointer hover:text-orange">
+    <>
+      <nav
+        className={
+          router.pathname === "/"
+            ? `fixed z-10 t-0 px-4 lg:px-0 inset-x-0 h-16 nunito ${
+                offsetY >= 32
+                  ? "bg-white text-blue-900 border-b-2 border-gray-300"
+                  : "bg-white text-blue-900 lg:bg-transparent lg:text-white"
+              }`
+            : "fixed z-10 t-0 px-4 lg:px-0 inset-x-0 h-16 nunito bg-white text-blue-900 border-b-2 border-gray-300"
+        }
+        style={{
+          transition: `background-color ease-out .25s`
+        }}
+      >
+        <div className="max-w-5xl flex items-center m-auto h-full">
+          <h1 className="font-extrabold text-xl lg:text-2xl flex-1">
             <Link href="/">
-              <a
-                title="Beranda"
-                className={
-                  router.pathname === "/" ? "active font-semibold" : ""
-                }
+              <a title="Beranda">SMKN 3 Jember</a>
+            </Link>
+          </h1>
+          <div className="hidden lg:grid grid-cols-5 justify-center items-center gap-4 tracking-wider">
+            <div className="flex items-center justify-center cursor-pointer hover:text-orange">
+              <Link href="/">
+                <a
+                  title="Beranda"
+                  className={
+                    router.pathname === "/" ? "active font-semibold" : ""
+                  }
+                >
+                  Beranda
+                </a>
+              </Link>
+            </div>
+            <div className="flex items-center justify-center cursor-pointer hover:text-orange">
+              <Link href="/berita">
+                <a
+                  title="Berita"
+                  className={
+                    router.pathname === "/berita" ? "active font-semibold" : ""
+                  }
+                >
+                  Berita
+                </a>
+              </Link>
+            </div>
+            <div className="flex items-center justify-center cursor-pointer hover:text-orange">
+              <Link href="/profil/jurusan">
+                {/* prettier-ignore */}
+                <a
+                title="Jurusan"
+                className={ router.pathname === "/profil/jurusan" ? "active font-semibold" : "" }
               >
-                Beranda
+                Jurusan
               </a>
+              </Link>
+            </div>
+            <div className="group relative flex items-center justify-center cursor-pointer">
+              <span className="mr-2">Profil</span>
+              <Arrow />
+              <div
+                className="group-hover:block hidden absolute w-64 pt-6"
+                style={{ right: `-0.5rem`, top: `50%` }}
+              >
+                <div className="grid grid-rows-3 bg-white text-blue-900 p-4 rounded-md border-2 border-gray-300">
+                  <Link href="/profil/sekolah">
+                    <a className="nunito block hover:bg-lightOrange hover:text-darkOrange rounded-md p-2">
+                      Sekolah
+                    </a>
+                  </Link>
+                  <Link href="/profil/staf-dan-guru">
+                    <a className="nunito block hover:bg-lightOrange hover:text-darkOrange rounded-md p-2">
+                      Staf dan Guru
+                    </a>
+                  </Link>
+                  <Link href="/profil/komite-sekolah">
+                    <a className="nunito block hover:bg-lightOrange hover:text-darkOrange rounded-md p-2">
+                      Komite Sekolah
+                    </a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="group relative flex items-center justify-center cursor-pointer">
+              <span className="mr-2">LSP</span>
+              <Arrow />
+              <div
+                className="group-hover:block hidden absolute w-64 pt-6"
+                style={{ right: `-0.5rem`, top: `50%` }}
+              >
+                <div className="grid grid-rows-3 bg-white text-blue-900 p-4 rounded-md border-2 border-gray-300">
+                  <Link href="/lsp/ketua-lsp">
+                    <a className="nunito block hover:bg-lightOrange hover:text-darkOrange rounded-md p-2">
+                      Ketua LSP
+                    </a>
+                  </Link>
+                  <Link href="/lsp/sejarah-singkat">
+                    <a className="nunito block hover:bg-lightOrange hover:text-darkOrange rounded-md p-2">
+                      Sejarah Singkat
+                    </a>
+                  </Link>
+                  <Link href="/lsp/skema-lsp">
+                    <a className="nunito block hover:bg-lightOrange hover:text-darkOrange rounded-md p-2">
+                      Skema LSP
+                    </a>
+                  </Link>
+                  <Link href="/lsp/visi-dan-misi">
+                    <a className="nunito block hover:bg-lightOrange hover:text-darkOrange rounded-md p-2">
+                      Visi dan Misi
+                    </a>
+                  </Link>
+                  <Link href="/lsp/struktur-organisasi">
+                    <a className="nunito block hover:bg-lightOrange hover:text-darkOrange rounded-md p-2">
+                      Struktur Organisasi
+                    </a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="block lg:hidden hamburger w-8 h-8 relative flex flex-col justify-center">
+            <input
+              type="checkbox"
+              className="absolute w-8 h-8 opacity-0"
+              onClick={() => setVisible(!isVisible)}
+            />
+            <span className="block w-full bg-blue-900 rounded"></span>
+            <span className="block w-full bg-blue-900 rounded my-2"></span>
+            <span className="block w-full bg-blue-900 rounded"></span>
+          </div>
+        </div>
+      </nav>
+
+      {isVisible && (
+        <div className="fixed mt-16 grid-rows-5 justify-center items-center tracking-wider bg-white z-50 w-full text-xl shadow-lg">
+          <div className="flex items-center justify-center cursor-pointer my-4">
+            <Link href="/">
+              <a title="Beranda">Beranda</a>
             </Link>
           </div>
-          <div className="flex items-center justify-center cursor-pointer hover:text-orange">
+          <div className="flex items-center justify-center cursor-pointer my-4">
             <Link href="/berita">
-              <a
-                title="Berita"
-                className={
-                  router.pathname === "/berita" ? "active font-semibold" : ""
-                }
-              >
-                Berita
-              </a>
+              <a title="Berita">Berita</a>
             </Link>
           </div>
-          <div className="flex items-center justify-center cursor-pointer hover:text-orange">
+          <div className="flex items-center justify-center cursor-pointer my-4">
             <Link href="/profil/jurusan">
               {/* prettier-ignore */}
               <a
@@ -68,14 +169,13 @@ export default function Navbar({ currentPage }) {
               </a>
             </Link>
           </div>
-          <div className="group relative flex items-center justify-center cursor-pointer">
-            <span className="mr-2">Profil</span>
-            <Arrow />
-            <div
-              className="group-hover:block hidden absolute w-64 pt-6"
-              style={{ right: `-0.5rem`, top: `50%` }}
-            >
-              <div className="grid grid-rows-3 bg-white text-blue-900 p-4 rounded-md border-2 border-gray-300">
+          <div className="group relative flex flex-col items-center justify-center cursor-pointer my-4">
+            <div className="flex items-center">
+              <span className="mr-2">Profil</span>
+              <Arrow />
+            </div>
+            <div className="group-hover:block hidden w-64 text-center">
+              <div className="grid grid-rows-3 bg-white text-blue-900 p-4">
                 <Link href="/profil/sekolah">
                   <a className="nunito block hover:bg-lightOrange hover:text-darkOrange rounded-md p-2">
                     Sekolah
@@ -94,14 +194,13 @@ export default function Navbar({ currentPage }) {
               </div>
             </div>
           </div>
-          <div className="group relative flex items-center justify-center cursor-pointer">
-            <span className="mr-2">LSP</span>
-            <Arrow />
-            <div
-              className="group-hover:block hidden absolute w-64 pt-6"
-              style={{ right: `-0.5rem`, top: `50%` }}
-            >
-              <div className="grid grid-rows-3 bg-white text-blue-900 p-4 rounded-md border-2 border-gray-300">
+          <div className="group relative flex flex-col items-center justify-center cursor-pointer my-4">
+            <div className="flex items-center">
+              <span className="mr-2">LSP</span>
+              <Arrow />
+            </div>
+            <div className="group-hover:block hidden w-64 text-center">
+              <div className="grid grid-rows-3 bg-white text-blue-900 p-4">
                 <Link href="/lsp/ketua-lsp">
                   <a className="nunito block hover:bg-lightOrange hover:text-darkOrange rounded-md p-2">
                     Ketua LSP
@@ -131,7 +230,7 @@ export default function Navbar({ currentPage }) {
             </div>
           </div>
         </div>
-      </div>
-    </nav>
+      )}
+    </>
   )
 }
