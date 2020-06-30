a2f78acd972eaa0d8193a2273ab83bb7152bbf1f
elianiva <dicha.arkana03@gmail.com>
2020-06-30 13:21:26 +0700

    limit items for News section on homepage

--- parts/News.js
+++ parts/News.js
@@ -9,7 +9,7 @@ export default function News({ data }) {
     <div className="m-auto mb-8">
       <Title title="Berita dan Informasi" />
       <div className="grid grid-cols-3 gap-4 mb-4">
-        {data.map((news, index) => (
+        {data.slice(0, 3).map((news, index) => (
           <NewsCard
             imgUrl={news.cover}
             title={news.title}
