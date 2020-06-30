6634305f9b1d01284f3ffb0fb16870484cbe21bb
elianiva <dicha.arkana03@gmail.com>
2020-06-30 13:21:33 +0700

    added News page

--- pages/berita.js
+++ pages/berita.js
@@ -3,29 +3,37 @@ import { id } from "date-fns/locale"
 import Title from "components/Title"
 import Button from "components/Button"
 import NewsCard from "components/NewsCard"
-import NewsCard from "components/NewsCard"
+import Breadcrumb from "components/Breadcrumb"
 import Layout from "components/Layout"
+import { getSortedNews } from "lib/berita"
 
-export default function News({ data }) {
+export default function NewsPage({ allNews }) {
   return (
-    <div className="m-auto mb-8">
-      <Title title="Berita dan Informasi" />
-      <div className="grid grid-cols-3 gap-4 mb-4">
-        {data.map((news, index) => (
-          <NewsCard
-            imgUrl={news.cover}
-            title={news.title}
-            desc={news.excerpt}
-            date={format(parseISO(news.date), "dd MMMM yyyy", { locale: id })}
-            key={index}
-          />
-        ))}
+    <Layout title="Berita">
+      <div className="max-w-5xl m-auto pt-24">
+        <Breadcrumb />
+        <Title title="Berita dan Informasi" />
+        <div className="grid grid-cols-3 gap-4 mb-16">
+          {allNews.map((news, index) => (
+            <NewsCard
+              imgUrl={news.cover}
+              title={news.title}
+              desc={news.excerpt}
+              date={format(parseISO(news.date), "dd MMMM yyyy", {
+                locale: id
+              })}
+              key={index}
+            />
+          ))}
+        </div>
       </div>
-      <Button
-        href="/berita"
-        className="block m-auto w-40 text-center"
-        text="Berita lainnya"
-      />
-    </div>
+    </Layout>
   )
+}
+
+export async function getStaticProps() {
+  let allNews = getSortedNews()
+  return {
+    props: { allNews }
+  }
 }
