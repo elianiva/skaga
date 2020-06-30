d0b62d4b51f6bd01b259ff12ec95a2a03992c2c9
elianiva <dicha.arkana03@gmail.com>
2020-06-30 19:46:22 +0700

    optimized Image component

--- components/Image.js
+++ components/Image.js
@@ -1,17 +1,5 @@
 export default function Image({ src, className, alt }) {
   return (
-    <div className={className}>
-      <img
-        className="blurred"
-        src={require(`images/${src}?lqip`)}
-        alt="placeholder"
-      />
-      <img src={require(`images/${src}?webp`)} alt={alt} />
-      <style jsx>{`
-        .blurred {
-          blur(2rem);
-        }
-      `}</style>
-    </div>
+    <img src={require(`images/${src}?webp`)} alt={alt} className={className} />
   )
 }
