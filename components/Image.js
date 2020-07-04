6bd8cd349dff76fd7b99dd3e8228aaae0835b198
elianiva <dicha.arkana03@gmail.com>
2020-07-04 07:15:36 +0700

    lazyload the image

--- components/Image.js
+++ components/Image.js
@@ -1,5 +1,10 @@
 export default function Image({ src, className, alt }) {
   return (
-    <img src={require(`images/${src}?webp`)} alt={alt} className={className} />
+    <img
+      src={require(`images/${src}?webp`)}
+      alt={alt}
+      className={className}
+      loading="lazy"
+    />
   )
 }
