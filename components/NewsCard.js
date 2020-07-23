654458d6e3f782dc3de1d9f24c3697e01334d82b
elianiva <dicha.arkana03@gmail.com>
2020-07-23 18:12:51 +0700

    fixed hard reload

--- components/NewsCard.js
+++ components/NewsCard.js
@@ -6,7 +6,7 @@ export default function NewsCard({ imgUrl, title, desc, date, href }) {
     <div className="flex flex-col rounded-md border border-gray-400 overflow-hidden">
       <Image src={imgUrl} alt={title} className="h-48 object-cover" />
       <div className="flex flex-col items-start p-4">
-        <Link href={href} as={`/berita/${href}`}>
+        <Link href="/berita/[id]" as={`/berita/${href}`}>
           <a title="Blog">
             <h1 className="text-lg nunito mb-2">{title}</h1>
           </a>
