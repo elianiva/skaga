05b630e5bd52586091e018e84231d10de1860ed9
elianiva <dicha.arkana03@gmail.com>
2020-10-04 19:00:14 +0700

    chore: cleanup svelte branch

--- components/Title.js
+++
@@ -1,8 +0,0 @@
-export default function Title({ title }) {
-  return (
-    <div className="flex flex-col items-center mt-4 mb-8">
-      <h1 className="text-3xl nunito text-center">{title}</h1>
-      <div className="h-1 w-40 bg-orange rounded"></div>
-    </div>
-  )
-}
--- pages/_app.js
+++
@@ -1,6 +0,0 @@
-import "styles/index.css"
-import "react-responsive-carousel/lib/styles/carousel.min.css"
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
